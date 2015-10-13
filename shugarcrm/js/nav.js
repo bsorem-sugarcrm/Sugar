f9e28d20de0ff6b4e7adf7869cbda7d4f5ea751f
Kristian Arlt <kristian.arlt@gmail.com>
2015-10-13 18:15:23 +0200

    Added custom nav init

--- shugarcrm/js/nav.js
+++ shugarcrm/js/nav.js
@@ -284,6 +284,33 @@ var Tree = (function () {
             $(this).toggleClass('open');
         });
     };
+    Tree.prototype.getPathUntilDepth = function(path, depth){
+        var result = "";
+        var i = 0;
+        while(path[i] != undefined && path[i].length > 0){
+            result += "/" + path[i];
+            i++;
+            if(i == depth)
+                break;
+        }
+        return result;
+    };
+    Tree.prototype.getHeaderTags = function(){
+        var order = 0;
+        var tags = [];
+        $('.content-body h2').each(function() {
+            var node = { 
+                children : [],
+                href : "#" + $(this).attr('id'), 
+                name : $(this).text(),
+                order: order,
+                sort : "m"
+            };
+            order++;
+            tags.push(node);
+        });
+        return tags;
+    };
     Tree.prototype.setTreeTitle = function(title){
 
     };
@@ -351,11 +378,61 @@ NavTree = new Tree();
                 jsonp: false,
                 jsonpCallback: 'sitemap'
             }).done(function (tree) {
-                var root = getUrl(window.location.href);
-                NavTree.setData(tree);
-                var content = document.querySelector('#tree-navigation-content .widget-body');
-                NavTree.init(root);
-                NavTree.setHover();
+
+                var url = getUrl(window.location.href);
+                var path = url.replace(/^https?:\/\/[^\/]+\//i, "").replace(/\/$/, "");
+                var pathArr = path.split("/");
+
+                var searchPath = "";
+                // https://support.sugarcrm.com/Documentation/Sugar_Versions/7.6/Ent/Application_Guide/Getting_Started
+                if(pathArr[0] == "Get Started"){
+
+                }else if(pathArr[0] == "Documentation"){
+                    if(pathArr[1] == "Sugar_Versions"){
+                        searchPath = NavTree.getPathUntilDepth(pathArr, 5);
+                    }else if(pathArr[1] == "Mobile_Solutions"){
+
+                    }else if(pathArr[1] == "Plug_ins"){
+
+                    }else if(pathArr[1] == "Installable_Connectors"){
+
+                    }else if(pathArr[1] == "Sugar_Developer"){
+
+                    }
+                }else if(pathArr[0] == "Knowledge_Base"){
+
+                }
+
+                var treeData = tree;
+                //This will go into the Tree if on live site
+                // if(treeData == null)
+                    // treeData = NavTree.getData();
+                
+                var branch = NavTree.findKey({ "href" : searchPath }, treeData);
+
+                //Get top-level sibling nodes
+                var searchPathParent = searchPath.substring(0, searchPath.lastIndexOf("/"));
+                var branchParent = NavTree.findKey({ "href" : searchPathParent }, treeData);
+                var siblingList = NavTree.createSiblingList(branchParent.children, branch.name);
+
+                
+                if(branch){
+                    NavTree.addToc(branch, "/"+path, getHeaderTags());
+                    // NavTree.setTreeTitle(branch.name);
+                    NavTree.setData(branch);
+                    var content = document.querySelector('#tree-navigation-content .widget-body');
+                    NavTree.init(url);
+                    NavTree.setHover();
+
+                    $('#tree-title').append(siblingList);
+                }
+
+                
+                // var root = getUrl(window.location.href);
+                // NavTree.setData(tree);
+                // var content = document.querySelector('#tree-navigation-content .widget-body');
+                // NavTree.init(root);
+                // NavTree.setHover();
             });
         }
     });
