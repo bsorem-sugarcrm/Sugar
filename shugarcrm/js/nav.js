962920b42086dbf2320cea6c9fbf9ec40781711a
Kristian Arlt <kristian.arlt@gmail.com>
2015-10-22 15:53:43 +0200

    arr

--- shugarcrm/js/nav.js
+++ shugarcrm/js/nav.js
@@ -439,7 +439,7 @@ var Tree = (function () {
         }else if(pathArr[0] == "Documentation"){
             if(pathArr[1] == "Sugar_Versions"){
                 if(pathArr.length >= 5){
-                    path2 = this.getPathUntilDepth(path, 5);
+                    path2 = this.getPathUntilDepth(pathArr, 5);
                 }
             }else if(pathArr[1] == "Installable_Connectors"){
 
